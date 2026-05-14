aabd3e0c3cfc16bc7ce0101eb36fa28ef3366946
hsnfszszzs1 <lingchapukk2@gmail.com>
2026-05-15 01:41:11 +0300

    refactor(generators): Create generators/character_generator.py

---
+++ generators/character_generator.py
@@ -0,0 +1,12 @@
+"""
+RGFLAT Generators - Character Generator
+"""
+
+# Placeholder / To be expanded with advanced character generation logic
+
+def generate_character(preset: str = None, **kwargs):
+    return {
+        "name": kwargs.get("name", "Generated Character"),
+        "personality": kwargs.get("personality", "mysterious"),
+        "background": kwargs.get("background", "")
+    }
